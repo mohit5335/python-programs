e039ace4b0ff14791aa0135085fd103b0afcc49e
mohit5335 <mohitsanwal53@gmail.com>
2019-02-18 23:55:41 +0530

    Sorting Array in Increasing Order

---
+++ sortarray2.py
@@ -0,0 +1,22 @@
+#initailize array
+a = [4,7,2,8,9,2,3,1]
+temp = 0
+
+#Original Array
+print("Elements of Original array : ")
+for i in range (0,len(a)):
+    print(a[i])
+    
+#Sort array in ascending order
+for i in range(0, len(a)):    
+    for j in range(i+1, len(a)):    
+        if(a[i] > a[j]):    
+            temp = a[i];    
+            a[i] = a[j];    
+            a[j] = temp;    
+     
+
+#Displaying elements of array after sorting    
+print("Elements of array sorted in ascending order: ")    
+for i in range(0, len(a)):     
+    print(a[i]),   
