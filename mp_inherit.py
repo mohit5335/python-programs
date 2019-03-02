3d7e3f7989fa490da63c0de5b1ea39f1454d1b1b
mohit5335 <mohitsanwal53@gmail.com>
2019-03-03 00:30:18 +0530

    Multiple Inheritance

---
+++ mp_inherit.py
@@ -0,0 +1,30 @@
+#program to see the concept of multiple inheritance in class
+
+class A:
+    def __init__(self):
+        print("I am in constructor of class A")
+
+    def fun1(self):
+        print("I am in class A")
+
+class B(A):                                    #class B is inheriting class A
+    def __init__(self):                        
+        super().__init__()                          #calls constructor of A
+        print("I am in constructor of class B")     
+        
+    def fun2(self):
+        print("I am in class B")
+
+class C(B,A):                                    #class C is inheriting class B
+    def __init__(self):
+        super().__init__()                     #calls constructor of B    
+        print("I am in constructor of class C")     
+        
+    def fun3(self):
+        print("I am in class C")
+
+
+c = C()         #object of C
+c.fun1()
+c.fun2()
+c.fun3()
